2271ea944ab35f053b0e54fdc6ad41aef49a3086
spais <maksim.ryabcew@gmail.com>
2020-09-29 09:49:21 +0500

    second

---
+++ parentheses/parentheses.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Rus");
+	int balance=0;
+	string x;
+	cin >> x;
+	for (int i = 0; i < x.length(); i++)
+	{
+		if (x[i] == '{')
+			balance += 1;
+		if (x[i] == '}')
+			balance -= 1;
+	}
+	if (balance == 0)
+		cout << "Баланс соблюден";
+	else
+		cout << "Баланс не соблюден";
+	return 0;
+}
