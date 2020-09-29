31ef4478372be66069cff28a8aadea8e648807a2
spais <maksim.ryabcew@gmail.com>
2020-09-29 09:23:52 +0500

    first

---
+++ watermelon/watermelon.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    int watermelon_weight;
+    std::cin >> watermelon_weight;
+    if (watermelon_weight > 2 && watermelon_weight % 2 == 0)
+        std::cout << "Yes";
+    else
+        std::cout << "No";
+    return 0;
+}
+
